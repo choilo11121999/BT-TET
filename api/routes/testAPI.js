3502de92b5e845276a3dce7c7b328d5a23ac4ae5
Chọi Lò <17020613@vnu.edu.vn>
2021-02-23 14:47:46 +0700

    Revert "refs #1 Tao bieu do tron"
    This reverts commit 4359b2f4445b466d9485f63a3c8f5b6131a3fdc1.

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,14 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var ios = Math.floor(Math.random() * 1000);
-var android = Math.floor(Math.random() * 1000);
-
 router.get('/', function(req, res, next) {
-    res.send({
-        ios: ios,
-        android: android
-    });
+    res.send("API test is working!");
 });
 
 module.exports = router;
