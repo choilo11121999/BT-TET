7f424e4e04a40c6cb8dd1534a2fac0cbc944fd5c
Chọi Lò <17020613@vnu.edu.vn>
2021-02-23 15:25:16 +0700

    refs #1 Tao bieu do tron

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+var ios = Math.floor(Math.random() * 1000);
+var android = Math.floor(Math.random() * 1000);
+
 router.get('/', function(req, res, next) {
-    res.send("API test is working!");
+    res.send({
+        ios: ios,
+        android: android
+    });
 });
 
-module.exports = router;
+module.exports = router; 
