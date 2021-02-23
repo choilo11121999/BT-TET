4359b2f4445b466d9485f63a3c8f5b6131a3fdc1
Chọi Lò <17020613@vnu.edu.vn>
2021-02-23 14:07:07 +0700

    refs #1 Tao bieu do tron

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import React, { Component } from 'react';
+import { VictoryPie } from 'victory'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      apiResponse: ''
+      ios: '',
+      android: ''
     };
   }
 
   callApi() {
     fetch('http://localhost:9000/api')
-      .then(res => res.text())
-      .then(res => this.setState({apiResponse: res}));
+      .then(res => res.json())
+      .then(res => this.setState({
+        ios: res.ios,
+        android: res.android
+      }));
   }
 
   componentDidMount() {
@@ -20,22 +25,48 @@ class App extends Component {
   }
 
   render() {
+    const { ios, android } = this.state
+    const iosPer = Math.round(ios / (ios + android) * 100);
+    const androidPer = Math.round(android / (ios + android) * 100);
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.apiResponse}</p>
+        <h3>Device Type</h3>
+       <div style={{
+         display: "flex",
+         alignItems: "center"
+       }}>
+        <svg width={400} height={300}>
+            <VictoryPie
+              colorScale = {["cyan", "purple"]} 
+              standalone={false}
+              width={300} height={300}
+              innerRadius={75}
+              data={[
+                { x: "ios", y: iosPer },
+                { x: "android", y: androidPer }
+              ]}
+            />
+          </svg>
+          <div>
+            <table>
+              <tr>
+                <td>
+                  <span id="ios"></span>iOS: 
+                  <div style={{marginLeft: "40px", fontSize: "20px"}}>{iosPer}%</div>
+                </td>
+                <td>{ios}</td>
+              </tr>
+              <tr>
+                <td>
+                  <span id="android"></span>Android: 
+                  <div style={{marginLeft: "40px", fontSize: "20px"}}>{androidPer}%</div>
+                </td>
+                <td>{android}</td>
+              </tr>
+            </table>
+          </div>
+       </div>
       </div>
     );
   }
