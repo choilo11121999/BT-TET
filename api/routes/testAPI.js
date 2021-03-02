1de84898881f9467cdcc6f748d0cd4f967898c20
Chọi Lò <17020613@vnu.edu.vn>
2021-03-02 10:14:20 +0700

    xoa dong trong va dau cach

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,13 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-
-
-router.get('/', function(req, res, next) {
-    const ios = Math.floor(Math.random() * 1000);
-    const android = Math.floor(Math.random() * 1000);
-    const data = [ios, android];
-    res.send(data);
+router.get("/", function (req, res, next) {
+  const ios = Math.floor(Math.random() * 1000);
+  const android = Math.floor(Math.random() * 1000);
+  const data = [ios, android];
+  res.send(data);
 });
 
-module.exports = router; 
+module.exports = router;
